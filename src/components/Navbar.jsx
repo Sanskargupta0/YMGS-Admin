a4f0a556848515ccc386d9e05eb5c7ce9c616be5
Sanskargupta0 <sanskar362002@gmail.com>
2025-03-14 11:13:36 +0530

    Added:- logo and svg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
-
-
-
-
+import { assets } from '../assets/assets'
 const Navbar = ({setToken}) => {
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
-      <p className="font-display text-2xl text-primary">
-            YMGS pharmacy Admin Dashboard
-          </p>
+  <div className="flex items-center gap-2 font-display text-2xl text-primary">
+    <span className="text-lg sm:text-2xl">YMGS pharmacy Admin Dashboard</span>
+    <img 
+      src={assets.logo} 
+      alt="YMGS Pharmacy Logo" 
+      className="h-auto w-auto max-w-[30px] max-h-[30px] sm:max-w-[40px] sm:max-h-[40px]" 
+    />
+  </div>
+
       <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
